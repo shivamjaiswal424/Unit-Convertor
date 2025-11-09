d014c981c660649f5716a8c91cd8a52a6f521ffe
shivamjaiswal424 <shivam424.sj@gmail.com>
2025-11-09 14:16:54 +0530

    new project

---
+++ src/main/java/com/example/Unit_Convertor/UnitConvertorApplication.java
@@ -0,0 +1,13 @@
+package com.example.Unit_Convertor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnitConvertorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UnitConvertorApplication.class, args);
+	}
+
+}
